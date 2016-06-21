76861877c3e3add743c788851c38fe8842f64c74
hnnyzyf <hnnyzyf2013@163.com>
2016-06-22 06:03:56 +0800

    6.21.22.03

---
+++ kernel/debug/printf.c
@@ -0,0 +1,20 @@
+/*###########################################
+# File Name:printf.c
+# Author:YFzhang
+# mail:hnnyzyf2013@163.com
+# Created Time:Wed 22 Jun 2016 05:58:26 AM CST
+# Last ModifiedWed 22 Jun 2016 05:58:26 AM CST
+##########################################*/
+
+
+#include "console.h"
+#include "common.h"
+#include "debug.h"
+#include "vargs.h"
+
+
+//实现pritnf函数
+void printf(const char *format,...)
+{
+
+}
