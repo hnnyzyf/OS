df6dd526fa79c1d161b5e8ce706324045a63e91e
hnnyzyf <hnnyzyf163@163.com>
2016-09-07 18:44:40 +0800

    9.7.10.44

--- kernel/debug/printf.c
+++ kernel/debug/printf.c
@@ -3,7 +3,7 @@
 # Author:YFzhang
 # mail:hnnyzyf2013@163.com
 # Created Time:Wed 22 Jun 2016 05:58:26 AM CST
-# Last Modified:Sat 25 Jun 2016 09:37:13 AM CST
+# Last Modified:2016年09月07日 星期三 18时10分20秒
 ##########################################*/
 
 
@@ -11,9 +11,6 @@
 #include "common.h"
 #include "debug.h"
 #include "vargs.h"
-
-
-
 
 
 //函数流程
@@ -24,12 +21,11 @@ void printf(const char *format,...)
 	//声明一个静态变量，生存周期是在该函数内
 	static char buff[2048];
 	va_list va;
-	char *format_temp=format;
 	//指向第一个参数
 	va_start(va,format);
 	//停止参数读取的终点是format
 	//将每一个参数写入buff中
-	vsprintf(buff,format,va);
+	vsprintk(buff,format,va);
 	va_end(va);
 	console_write(buff);
 }
@@ -62,144 +58,139 @@ void printf_color(real_color_t back,real_color_t fore,const char *foramt,...)
 #define dot			 4;//根据精度输出固定的长度,1为存在，0为不存在
 
 
-//该函数用来将内容赋予buff
-static void content(char *buff,unsigned char data_format,pr_type_t type,int word_width,int precision,va_list va)
-{
-	//用来记录每一个数字位的数据
-	char digit[16]="0123456789ABCDEF";
-	char temp[32]
-	int temp1;
-	//如果是有符号数，默认需要判断类型，是否输出-号
-	if(type==pr_int)
-	{
-		temp1=va_va_arg(va,int);
-
-	}
-	
-
-}
-
-
-
 
 
 //10进制获得对应n进制每一位的数据
 
+//该函数是处理format，根据format来获得要输出几个参数，输出的样式，以及类型
 
-
-
-
-static void vsprintf(char *buff,char *format,va_list va)
+static void vsprintk(char *buff,const char *format,va_list va)
 {
-	char * buff_temp=buff;
-	//记录输出字符的宽度,默认为0
-	int word_width=0;
-	//记录输出字符的精度,-1表示默认精度
+	//记录输出字符的宽度,-1表示宽度未定义
+	int word_width=-1;
+	//记录输出字符的精度,-1表示精度未定义
 	int precision=-1;
 	//记录要输出的数据的格式
-	unsigned char int data_format=0;
+	unsigned char data_format=0;
+	//记录buff中最后的一个字符所在位置
+	int location=-1;
 	//中止的条件,是format字符串到达末尾
 	while(*format!='\0')
 	{
 		//首先判断是不是%号，不是的话则将其放入到buff中
 		if(*format!='%')
 		{
-			*buff_temp++=*format;
-			format++;
+			buff[++location]=*format++;
+			
+			//#if PRINTF_MARCO==1
+			//	printf("%c+++1\n",buff[location]);
+			//#endif
+		
 		}
 		//如果是%号的话,则开始读取下一个字符
 		else
 		{
+			//指向下一个字符
 			format++;
-			//在指定地方跳出循环
-			
-			//%后的判断步骤
-			//1.首先判断格式控制符 -
-			//2.然后判断-后的格式是1.1还是5的格式
-			//3.然后判断
-			//开始判断下一个字符是什么
-			
 			//---------------------------------------------输出格式部分判断------------------------------
-			//存在对齐符号
-			if(*format=='-')
-			{
-				data_format=data_format+left;
-				//移动到下一个字符字符
-				format++;
-			}
-			//如果存在数字，需要设置宽度
-			if(*format>='0'&& *format<='9')
-			{
-				word_width=*format;
-				data_format=data_format+width;
-				format++;
-			}
-			//设置完宽度，需要看下一位的精度
-			if(*format=='.')
-			{
-				data_format=data_format+dot;
-				format++;
-				//获得小数的精度信息
-				precision=*format;
-				format++;
-			}
 			//---------------------------------------------输出类型部分判断------------------------------
-			switch(*format)
-			{
-				//输出int类型的十进制数
-				case 'd':
-				case 'i':
-					content(buff_temp,data_foramt,pr_int,word_width,precision,va);
-					break;
-				//输出无符号的8进制数
-				case 'o':
-					break;
-				//输出无符号的十六进制数
-				case 'x':
-				case 'X':
-					break;
-				//输出无符号的十进制数
-				case 'u':
-					break;
-				//输出单个字符
-				case 'c':
-					break;
-				//输出字符串
-				case 's':
-				case 'S':
-					break;
-				//双精度类型，默认为6
-				case 'f':
-					break;
-
-			}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		}
-
+			//格式化
+			location=content(buff,*format,location,va,data_format,word_width,precision);
+			//指向下一个字符
+			//#if PRINTF_MARCO==1
+				//printf("%c+++2\n",*format);
+			//#endif
+			format++;
 		}
 	}
+	//字符串终结
+	buff[++location]='\0';
+	//printf("%s\n",buff);
 }
 
+//根据每一个%后的内容填入buff中
+static int content(char *buff,char type,int location,va_list va,unsigned char data_format,int word_width,int precision)
+{
+	char word[]="0123456789abcdef";
+	char *temp=NULL;
+	//--------------------------------根据输出参数判断是否需要格式化--------------------------------------
 
+	//--------------------------------根据输入参数的类型填入buff要输出的内容------------------------------
+	switch(type)
+	{
+		//输出类型是int型
+		case 'd':
+		location=itoc(buff,va_arg(va,int),10,word,location);
+		break;
+		//输出类型是十六进制
+		case 'x':
+		case 'X':
+		location=itoc(buff,va_arg(va,int),16,word,location);
+		break;
+		//输出类型是字符
+		case 'c':
+		buff[++location]=va_arg(va,int);
+		break;
+		//输出类型是字符串
+		case 's':
+		temp=va_arg(va,char *);
+		while(*temp!='\0')
+		{
+			buff[++location]=*temp++;			
+		}
+		break;
+		default:break;
+	}
+	return location;
+}
 
-
-
+//int根据不同进制字符串
+static int itoc(char *buff,int src,int radix,char word[],int location)
+{
+	//字符串原地逆序
+	int start=location+1;
+	int end=start;
+	//src标准化
+	if(src<0)
+	{
+		buff[++location]='-';
+		src=0-src;
+		start++;
+	}
+	else if(src==0)
+	{
+		buff[++location]='0';
+		return location;
+	}
+	else
+	{
+		src=src;
+	}
+	//十六进制加0x
+	if(radix==16)
+	{
+		buff[++location]='0';
+		buff[++location]='X';
+		start=start+2;
+	}
+	//记录起始位置
+	while(src!=0)
+	{
+		//将余数插入到buff中
+		buff[++location]=word[src%radix];
+		src=src/radix;
+	}
+	//原地逆序
+	end=location;
+	//中止条件是start=end或者start>end
+	while(start<end)
+	{
+		buff[location+1]=buff[start];
+		buff[start++]=buff[end];
+		buff[end--]=buff[location+1];
+	}
+	return location;
+}
 
 
 
